2c7cfe6de38b0971e3b24b634bd94c8011dd6f11
Adriano Anacleto <anacleto8000@gmail.com>
2024-11-20 18:16:11 -0300

    meu primeiro commit
    Desenvolvendo um site ordem livros formaçãoDEV

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+    import Logo from '../components/Template/Logo.svelte';
+	let { children } = $props();
+</script>
+<div
+class="
+flex flex-col justify-center items-center gap-5
+bg-black text-white min-h-screen">
+
+<Logo/>
+{@render children()}
+
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-3xl fonte-bold">Ordem dos Livros</h1>
+<a href="/autores">Autores</a>
+<div class="caixa"></div>
+
